d9deafa6c172a3e6b255594c89acd0710045c078
fajartd02 <fajarmh2@gmail.com>
2021-08-05 22:12:35 +0700

    Daily routine

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+int main() {
+	ll a,b;
+	std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std:: cin >> a >> b;
+    std :: cout << a + b << "\n";
+    return 0;
+}
